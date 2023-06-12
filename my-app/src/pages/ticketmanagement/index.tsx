d5b076053555315a0ed82244aa912df589804051
Namsweet <namdang23101998@gmail.com>
2023-06-12 16:26:48 +0700

    đẩy code lên ngày 12 tháng 6 năm 2023

--- my-app/src/pages/ticketmanagement/index.tsx
+++ my-app/src/pages/ticketmanagement/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import SideMenu from '../../component/Menubar/menubar';
+import Header from '../../component/Header/header';
+import { Input } from 'antd';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FiFilter } from 'react-icons/fi';
+
 
 function TicketManagement() {
 
   
   return(
-    <div>
+    <div className='app'>
     <SideMenu/>
-    <span className='home'> TicketManagement</span>
-    </div>
-  )
+    <div className='bodyhome'>
+      <Header/>
+      <div className='group1'>
+        <h1>Danh sách vé</h1>
+        <div className='group2'>
+        
+        <Input allowClear className='searchticker' placeholder='Tìm bằng số vé' style={{width: 437, height: 48}} suffix={<AiOutlineSearch fontSize={24}/>}/>
+          
+          <div className='filter'>
+            
+          <span><FiFilter  fontSize={24}/>Lọc vé</span>
+          </div>
+          <div className='exportfile'>
+            <span>Xuất file (.csv)</span>
+          </div>
+          </div>
+        </div>   
+        </div>
+      </div>  
+);
 }
 export default TicketManagement
